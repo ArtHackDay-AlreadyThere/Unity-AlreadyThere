a466243a27d7b128cfc61d2dd1ca6efcd59547cb
Toru Nayuki <tnayuki@icloud.com>
2018-03-03 21:03:20 +0900

    ブロック受信後にプレファブを生成するように

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+	[System.Serializable]
+	public struct Transaction {
+		public string From;
+		public string To;
+		public double Value;
+	}
+
+	public ulong Number;
+	public List<Transaction> Transactions;
+}
--- Assets/Scripts/Etherscan.cs
+++ Assets/Scripts/Etherscan.cs
@@ -19,18 +19,17 @@ public class Etherscan : MonoBehaviour {
 	public class GetBlockByNumberReturn {
 		public string jsonrpc;
 		public string id;
-		public Block result;
+		public GetBlockByNumberReturnResult result;
 	}
 
 	[System.Serializable]
-	public class Block {
+	public class GetBlockByNumberReturnResult {
 		public string number;
-		public Transaction[] transactions;
+		public GetBlockByNumberReturnResultTransaction[] transactions;
 	}
 
 	[System.Serializable]
-	public class Transaction {
-		public string blockHash;
+	public class GetBlockByNumberReturnResultTransaction {
 		public string from;
 		public string to;
 		public string value;
@@ -38,20 +37,64 @@ public class Etherscan : MonoBehaviour {
 
 	public string apiKey = "YourApiToken";
 
-	public List<Block> blocks;
+	public int initialLoadingCount = 20;
+
+	public GameObject prefab;
 
 	// Use this for initialization
 	IEnumerator Start () {
 		UnityWebRequest request = UnityWebRequest.Get("https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=" + apiKey);
 		yield return request.SendWebRequest();
 
-		var blockNumberResult = JsonUtility.FromJson<BlockNumberReturn>(request.downloadHandler.text);
+		var blockNumberReturn = JsonUtility.FromJson<BlockNumberReturn>(request.downloadHandler.text);
 
-		request = UnityWebRequest.Get("https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&apikey=" + apiKey + "&boolean=true&tag=" + blockNumberResult.result);
-		yield return request.SendWebRequest();
+		var currentBlockNumber = blockNumberReturn.result;
+		ulong currentBlockNumberUInt64 = System.Convert.ToUInt64(currentBlockNumber.Substring(2), 16);
 
-		var getBlockByNumberReturn = JsonUtility.FromJson<GetBlockByNumberReturn>(request.downloadHandler.text);
-		blocks.Add(getBlockByNumberReturn.result);
+		for (ulong blockNumber = currentBlockNumberUInt64 - (ulong)initialLoadingCount + 1; blockNumber <= currentBlockNumberUInt64; blockNumber++) {
+			request = UnityWebRequest.Get("https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&apikey=" + apiKey + "&boolean=true&tag=0x" + blockNumber.ToString("X"));
+			yield return request.SendWebRequest();
+
+			var getBlockByNumberReturn = JsonUtility.FromJson<GetBlockByNumberReturn>(request.downloadHandler.text);
+			GenerateBlock(getBlockByNumberReturn.result);
+		}
+
+		while (true) {
+			request = UnityWebRequest.Get("https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=" + apiKey);
+			yield return request.SendWebRequest();
+
+			blockNumberReturn = JsonUtility.FromJson<BlockNumberReturn>(request.downloadHandler.text);
+			if (!currentBlockNumber.Equals(blockNumberReturn.result)) {
+				request = UnityWebRequest.Get("https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&apikey=" + apiKey + "&boolean=true&tag=" + blockNumberReturn.result);
+				yield return request.SendWebRequest();
+
+				var getBlockByNumberReturn = JsonUtility.FromJson<GetBlockByNumberReturn>(request.downloadHandler.text);
+				GenerateBlock(getBlockByNumberReturn.result);
+			}
+
+			yield return new WaitForSeconds(5.0f);
+		}
 	}
-	
+
+	private void GenerateBlock(GetBlockByNumberReturnResult result) {
+		GameObject obj = Instantiate(prefab);
+
+		Block block = obj.AddComponent<Block>();
+		block.Number = System.Convert.ToUInt64(result.number.Substring(2), 16);
+
+		List<Block.Transaction> transactions = new List<Block.Transaction>(result.transactions.Length);
+		for (int i = 0; i < result.transactions.Length; i++) {
+			var transaction = new Block.Transaction();
+
+			transaction.From = result.transactions[i].from;			
+			transaction.To = result.transactions[i].to;	
+
+			string hex = result.transactions[i].value.Substring(2).PadLeft(32, '0');
+			transaction.Value = ((double)System.Convert.ToUInt64(hex.Substring(0, 16), 16) * 1.844674407370955e19 + (double)System.Convert.ToUInt64(hex.Substring(16, 16), 16)) / 1000000000000000000.0;
+
+			transactions.Add(transaction);
+		}
+
+		block.Transactions = transactions;
+	}
 }
